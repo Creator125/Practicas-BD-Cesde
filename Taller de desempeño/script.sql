3c65817470ebc8331436eeb523d688aa69017f2e
Creator125 <85368990+Creator125@users.noreply.github.com>
2023-05-08 11:45:44 -0500

    Nuevos proyectos

---
+++ Taller de desempeño/script.sql
@@ -0,0 +1,21 @@
+/*
+Se debe realizar una consulta por cada elemento del where.
+
+Utilizar la menos tres consultas con Inner Join
+
+Realizar consulta de campos calculados
+
+Realizar consulta con columnas agrupadas
+
+Realizar ejemplos de: Agregar columna, modificar el contenido, modificar el tipo, borrar y actualizar datos.
+*/
+
+
+
+
+
+
+
+
+
+
