f50489735168d98394670000a854fb9157bd7e4f
Creator125 <85368990+Creator125@users.noreply.github.com>
2023-05-31 20:38:32 -0500

    AgenciaViajes/: Se agregaron archivos SQL

---
+++ Procedimiento de almacenamiento/AgenciaViajes/SQL/Cordoba_Agualimpia_Obed_CreateAgencia.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE AgenciaViajes;
+USE AgenciaViajes;
+
+CREATE TABLE Sucursal(
+	codigoSucursal INT PRIMARY KEY,
+	direccion VARCHAR(100),
+	telefono VARCHAR(20)
+);
+
+CREATE TABLE Hotel(
+  codigoHotel INT PRIMARY KEY,
+  nombre VARCHAR(100),
+  direccion VARCHAR(100),
+  ciudad VARCHAR(50),
+  paginaWeb VARCHAR(100),
+  nHabitaciones INT,
+  precioPersona INT
+);
+
+CREATE TABLE Vuelos (
+  numeroVuelo INT PRIMARY KEY,
+  fechaHora DATETIME,
+  origen VARCHAR(50),
+  destino VARCHAR(50),
+  precioPersona DECIMAL(10, 2),
+  valorEquipaje DECIMAL(10, 2),
+  clase VARCHAR(10)
+);
+
+CREATE TABLE Turistas(
+  cedulaPasaporte VARCHAR(20) PRIMARY KEY,
+  nombre VARCHAR(20),
+  apellido VARCHAR(100),
+  correo VARCHAR(100),
+  celular VARCHAR(20),
+  codigoSucursal INT,
+  numeroVuelo INT,
+  CodigoHotel INT,
+  FOREIGN KEY (codigoSucursal) REFERENCES Sucursal(codigoSucursal),
+  FOREIGN KEY (numeroVuelo) REFERENCES Vuelos(numeroVuelo),
+  FOREIGN KEY (CodigoHotel) REFERENCES Hotel(CodigoHotel)
+);
